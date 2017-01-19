32978e02f3243772943bd119144d25d656e97030
Gary Tong <garytong@justin.tv>
2017-01-18 16:59:34 -0800

    Basic File IO

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 func main() {
+	if len(os.Args) == 1 {
+		panic("No file specified")
+	}
+	filename := os.Args[1]
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(string(buf))
 }
